e73b176234f309e2ffe7028cb4028be81a1c2237
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-08 23:57:05 -0500

    add Channels component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  <h1>Canales TV Peruana en vivo</h1>
+  <Channel />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import Channel from './components/Channel.vue';
 
 @Options({
   components: {
     HelloWorld,
+	Channel
   },
 })
 export default class App extends Vue {}
@@ -20,8 +24,8 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Channel.vue
@@ -0,0 +1,101 @@
+<template lang="">
+	<div>
+		<ul class="">
+			<li v-for="channel in channels" :key="channel">
+				<a :href="!channel.allowIframe ? channel.website : 'javascript:void(0)'"
+					:target="!channel.allowIframe ? '_blank' : '' ">
+					{{ channel.name }}
+				</a>	
+				<span v-if="!channel.allowIframe"> (Se abre en una nueva ventana)</span>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang = "ts">
+import { defineComponent } from 'vue';
+
+interface Channel {
+	name: string;
+	website: string;
+	number: any;
+	allowIframe: boolean;
+}
+
+const rawChannels = [
+	{
+		"name": "Latina",
+		"website": "https://www.latina.pe/tvenvivo",
+		"number": {
+			"national": "2",
+			"movistar-hd": "702"
+		},
+		"allowIframe": false
+	},
+	{
+		"name": "America TV",
+		"website": "https://tvgo.americatv.com.pe/",
+		"number": {
+			"national": "4",
+			"movistar-hd": "704"
+		},
+		"allowIframe": true
+	},
+	{
+		"name": "Panamericana",
+		"website": "https://panamericana.pe/tvenvivo",
+		"number": {
+			"national": "5",
+			"movistar-hd": "705"
+		},
+		"allowIframe": true
+	},
+	{
+		"name": "TV Perú",
+		"website": "https://www.tvperu.gob.pe/play",
+		"number": {
+			"national": "7",
+			"movistar-hd": "707"
+		},
+		"allowIframe": false
+	},
+	{
+		"name": "ATV",
+		"website": "https://www.atv.pe/envivo-atv",
+		"number": {
+			"national": "9",
+			"movistar-hd": "709"
+		},
+		"allowIframe": true
+	},
+	{
+		"name": "Willax",
+		"website": "https://willax.tv/en-vivo/",
+		"number": {
+			"national": "16",
+			"movistar-hd": "716"
+		},
+		"allowIframe": true
+	},
+];
+
+let channelsList: Channel[] = rawChannels;
+
+//console.log(channelsList);
+
+export default defineComponent({
+	name: 'Channel',
+	data() {
+		return {
+			channels: channelsList
+		}
+	},
+	mounted() {		
+		//console.log(this.channels);
+	},
+})
+
+</script>
+<style lang="">
+	
+</style>

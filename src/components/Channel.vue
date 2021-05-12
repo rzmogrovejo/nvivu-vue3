de6c3ce357682574a77ad20fce8e0d37690d5709
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-12 02:43:34 -0500

    support video stream for m3u8 format

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -2,34 +2,47 @@
 	<div>
 		<ul class="">
 			<li v-for="channel in channels" :key="channel">
-				<a :href="!channel.iframe.allow ? channel.url : 'javascript:void(0)'"
-					:target="!channel.iframe.allow ? '_blank' : '' "
-					@click="toggleIframe(channel)">
+				<a :href="!channel.stream.allow ? channel.url : 'javascript:void(0)'"
+					:target="!channel.stream.allow ? '_blank' : '' "
+					@click="toggleStream(channel)">
 					{{ channel.name }}
 				</a>	
-				<span v-if="!channel.iframe.allow"> (Se abre en una nueva ventana)</span>
+				<span v-if="!channel.stream.allow"> (Se abre en una nueva ventana)</span>
 			</li>
 			<br/>
-			<li><a href="javascript:void(0)" @click="offIframe()">Apagar</a></li>
+			<li><a href="javascript:void(0)" @click="offStream()">Apagar</a></li>
 		</ul>
-		<div v-if="showIframe" 
+<!-- 		<div v-if="showStream" 
 			style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;">
 			<iframe 
 				style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" 
 				frameborder="0" 
 				type="text/html" 
-				:src="iframeUrl" 
+				:src="streamUrl"
 				width="100%" 
 				height="100%"
 				allowfullscreen 
 				allow="autoplay">
 			</iframe>
+		</div> -->
+		<div v-if="showStream" 
+			style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;">
+<!-- 			<video 
+				ref="video"
+				style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" 
+				autoplay
+				controls>
+			</video> -->
+			<div v-html="streamHtml"></div>
 		</div>
 	</div>
 </template>
 
 <script lang = "ts">
+import Hls from 'hls.js';
 import { defineComponent } from 'vue';
+import { getStreamUrlWithToken } from '../helpers/atv';
+
 
 interface Channel {
 	"name": string;
@@ -38,9 +51,10 @@ interface Channel {
 		"national"?: string,
 		"movistar-hd"?: string
 	};
-	"iframe": {
-		"allow": boolean
-		"url"?: string,
+	"stream": {
+		"allow": boolean,
+		"type"?: string,
+		"url"?: string
 	}
 }
 
@@ -52,8 +66,8 @@ const rawChannels = [
 			"national": "2",
 			"movistar-hd": "702"
 		},
-		"iframe": {
-			"allow": false
+		"stream": {
+			"allow": false,
 		}
 	},
 	{
@@ -63,7 +77,7 @@ const rawChannels = [
 			"national": "4",
 			"movistar-hd": "704"
 		},
-		"iframe": {
+		"stream": {
 			"allow": false
 		}
 	},
@@ -74,8 +88,9 @@ const rawChannels = [
 			"national": "5",
 			"movistar-hd": "705"
 		},
-		"iframe": {
+		"stream": {
 			"allow": true,
+			"type": "iframe",
 			"url": "https://www.dailymotion.com/embed/video/x774s7s?autoplay=1"
 		}
 	},
@@ -86,7 +101,7 @@ const rawChannels = [
 			"national": "7",
 			"movistar-hd": "707"
 		},
-		"iframe": {
+		"stream": {
 			"allow": false,
 			"url": "https://iblups.com/e_tvperuHD"
 		}
@@ -98,8 +113,11 @@ const rawChannels = [
 			"national": "9",
 			"movistar-hd": "709"
 		},
-		"iframe": {
-			"allow": false
+		"stream": {
+			"allow": true,
+			"type": "video",
+			"url": "https://pe-atv-atv-live.ned.media/atv/smil:atv.smil/playlist.m3u8?iut=",
+			//"url": "https://pe-atv-atv-live.ned.media/atv/smil:atv.smil/playlist.m3u8?iut=eyJwYXJhbXMiOnsiZXhwIjoxNjIwODM5MDc3LCJzZXNzaW9uIjoiMTkwLjIzNy4xMjIuNjYiLCJ3aGl0ZWxpc3QiOlsiMTkwLjIzNy4xMjIuNjYiXX0sInNpZ25hdHVyZSI6ImMzZjMyZWYwNWZkYjMyMDhjNzE1OThkZDAyMmQ2NGUyNDlkNWI3ZDUifQ=="
 		}
 	},
 	{
@@ -109,8 +127,9 @@ const rawChannels = [
 			"national": "16",
 			"movistar-hd": "716"
 		},
-		"iframe": {
+		"stream": {
 			"allow": true,
+			"type": "iframe",
 			"url": "https://www.dailymotion.com/embed/video/x7x4dgx?autoplay=1"
 		}
 	},
@@ -118,34 +137,101 @@ const rawChannels = [
 
 let channelsList: Channel[] = rawChannels;
 
-//console.log(channelsList);
-
 export default defineComponent({
 	name: 'Channel',
 	data() {
 		return {
 			channels: channelsList,
-			showIframe: false,
-			iframeUrl: ""
+			showStream: false,
+			streamUrl: "",
+			streamHtml: ""
 		}
 	},
-	mounted() {		
-		//console.log(this.channels);
+	mounted() {
+
 	},
 	methods: {
-		toggleIframe(channel: Channel) {
-			this.showIframe = channel.iframe.allow;
-			if (channel.iframe.allow) {
-				this.iframeUrl = 'url' in channel.iframe ? channel.iframe.url! : channel.url;
+		toggleStream(channel: Channel) {
+			this.showStream = channel.stream.allow;
+
+			if (channel.stream.allow) {
+				this.streamUrl = 'url' in channel.stream ? channel.stream.url! : channel.url;	
+			}
+
+			let iframeTag: string = `
+				<iframe 
+					style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" 
+					frameborder="0" 
+					type="text/html" 
+					src="${ this.streamUrl }"
+					width="100%" 
+					height="100%"
+					allowfullscreen 
+					allow="autoplay">
+				</iframe>
+			`;
+
+			let videoTag: string = `
+				<video 
+					id="video"
+					style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;" 
+					autoplay
+					controls>
+				</video>
+			`;
+			
+			if (channel.stream.type == 'video') {
+				this.streamHtml = videoTag;
+				this.$nextTick(() => {
+					//let video: any = this.$refs["video"];
+					let video: any = document.getElementById("video");
+
+					if (channel.name == "ATV") {
+						getStreamUrlWithToken(this.streamUrl).then((streamUrl) => {
+							if (Hls.isSupported()) {
+								let hls = new Hls();
+								hls.loadSource(streamUrl);
+								hls.attachMedia(video);
+							} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+								video.src = streamUrl;
+							}
+						});
+					} else {
+						let stream = this.streamUrl;
+
+						if (Hls.isSupported()) {
+
+							let hls = new Hls();
+							hls.loadSource(stream);
+							hls.attachMedia(video);
+						} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+							video.src = stream;
+						}
+					}
+
+				})
+			} else if (channel.stream.type == 'iframe') {
+				this.streamHtml = iframeTag;
 			}
 		},
-		offIframe(){
-			this.showIframe = false;
+		offStream(){
+			this.showStream = false;
 		}
 	}
 })
 
 </script>
-<style lang="">
-	
+<style lang="css">
+
+#video::-webkit-media-controls-timeline {
+	display: none;
+}
+#video::-webkit-media-controls-current-time-display {
+	display: none;
+}
+#video::-webkit-media-controls-time-remaining-display {
+	display: none;
+}
+
+
 </style>

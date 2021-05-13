837e06833070c3264237d44bac40443de1c3e51c
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-12 19:36:25 -0500

    add data

---
+++ src/data/rawChannels.ts
@@ -0,0 +1,77 @@
+export default [
+	{
+		"name": "Latina",
+		"url": "https://www.latina.pe/tvenvivo",
+		"number": {
+			"national": "2",
+			"movistar-hd": "702"
+		},
+		"stream": {
+			"allow": false
+		}
+	},
+	{
+		"name": "America TV",
+		"url": "https://tvgo.americatv.com.pe/",
+		"number": {
+			"national": "4",
+			"movistar-hd": "704"
+		},
+		"stream": {
+			"allow": true,
+			"type": "iframe",
+			"url": "https://tvgo.americatv.com.pe/"
+		}
+	},
+	{
+		"name": "Panamericana",
+		"url": "https://panamericana.pe/tvenvivo",
+		"number": {
+			"national": "5",
+			"movistar-hd": "705"
+		},
+		"stream": {
+			"allow": true,
+			"type": "iframe",
+			"url": "https://www.dailymotion.com/embed/video/x774s7s?autoplay=1"
+		}
+	},
+	{
+		"name": "TV Perú",
+		"url": "https://www.tvperu.gob.pe/play",
+		"number": {
+			"national": "7",
+			"movistar-hd": "707"
+		},
+		"stream": {
+			"allow": false
+		}
+	},
+	{
+		"name": "ATV",
+		"url": "https://www.atv.pe/envivo-atv",
+		"number": {
+			"national": "9",
+			"movistar-hd": "709"
+		},
+		"stream": {
+			"allow": true,
+			"type": "iframe",
+			"url": "https://www.atv.pe/envivo-atv"
+			//"url": "https://pe-atv-atv-live.ned.media/atv/smil:atv.smil/playlist.m3u8?iut=",
+		}
+	},
+	{
+		"name": "Willax",
+		"url": "https://willax.tv/en-vivo/",
+		"number": {
+			"national": "16",
+			"movistar-hd": "716"
+		},
+		"stream": {
+			"allow": true,
+			"type": "iframe",
+			"url": "https://www.dailymotion.com/embed/video/x7x4dgx?autoplay=1"
+		}
+	},
+];

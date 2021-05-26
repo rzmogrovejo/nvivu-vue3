4fde121dacf742787ba5918bb17e60e6ba4626fe
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-25 19:12:56 -0500

    update data keys

--- src/data/rawChannels.ts
+++ src/data/rawChannels.ts
@@ -2,7 +2,7 @@ export default [
 	{
 		"name": "Latina",
 		"slug": "latina",
-		"url": "https://www.latina.pe/tvenvivo",
+		"source": "https://www.latina.pe/tvenvivo",
 		"number": {
 			"national": "2",
 			"movistar-hd": "702"
@@ -14,7 +14,7 @@ export default [
 	{
 		"name": "America TV",
 		"slug": "america-tv",
-		"url": "https://tvgo.americatv.com.pe/",
+		"source": "https://tvgo.americatv.com.pe/",
 		"number": {
 			"national": "4",
 			"movistar-hd": "704"
@@ -22,13 +22,13 @@ export default [
 		"content": {
 			"enable": false,
 			"type": "iframe",
-			"url": "https://tvgo.americatv.com.pe/"
+			"source": "https://tvgo.americatv.com.pe/"
 		}
 	},
 	{
 		"name": "Panamericana",
 		"slug": "panamericana",
-		"url": "https://panamericana.pe/tvenvivo",
+		"source": "https://panamericana.pe/tvenvivo",
 		"number": {
 			"national": "5",
 			"movistar-hd": "705"
@@ -36,13 +36,13 @@ export default [
 		"content": {
 			"enable": true,
 			"type": "iframe",
-			"url": "https://www.dailymotion.com/embed/video/x774s7s?autoplay=1"
+			"source": "https://www.dailymotion.com/embed/video/x774s7s?autoplay=1"
 		}
 	},
 	{
 		"name": "TV Perú",
 		"slug": "tv-peru",
-		"url": "https://www.tvperu.gob.pe/play",
+		"source": "https://www.tvperu.gob.pe/play",
 		"number": {
 			"national": "7",
 			"movistar-hd": "707"
@@ -54,7 +54,7 @@ export default [
 	{
 		"name": "ATV",
 		"slug": "atv",
-		"url": "https://www.atv.pe/envivo-atv",
+		"source": "https://www.atv.pe/envivo-atv",
 		"number": {
 			"national": "9",
 			"movistar-hd": "709"
@@ -62,14 +62,14 @@ export default [
 		"content": {
 			"enable": true,
 			"type": "video",
-			//"url": "https://www.atv.pe/envivo-atv"
-			"url": "https://pe-atv-atv-live.ned.media/atv/smil:atv.smil/playlist.m3u8?iut=",
+			//"source": "https://www.atv.pe/envivo-atv"
+			"source": "https://pe-atv-atv-live.ned.media/atv/smil:atv.smil/playlist.m3u8?iut=",
 		}
 	},
 	{
 		"name": "ATV Más",
 		"slug": "atv-mas",
-		"url": "https://www.atv.pe/envivo-atvmas",
+		"source": "https://www.atv.pe/envivo-atvmas",
 		"number": {
 			"national": "21",
 			"movistar-hd": "776"
@@ -77,14 +77,14 @@ export default [
 		"content": {
 			"enable": true,
 			"type": "video",
-			//"url": "https://www.atv.pe/envivo-atv"
-			"url": "https://pe-atv-atv-mas-live.ned.media/atv/smil:atv-mas.smil/playlist.m3u8?iut=",
+			//"source": "https://www.atv.pe/envivo-atv"
+			"source": "https://pe-atv-atv-mas-live.ned.media/atv/smil:atv-mas.smil/playlist.m3u8?iut=",
 		}		
 	},
 	{
 		"name": "Willax",
 		"slug": "willax",
-		"url": "https://willax.tv/en-vivo/",
+		"source": "https://willax.tv/en-vivo/",
 		"number": {
 			"national": "16",
 			"movistar-hd": "716"
@@ -92,7 +92,7 @@ export default [
 		"content": {
 			"enable": true,
 			"type": "iframe",
-			"url": "https://www.dailymotion.com/embed/video/x7x4dgx?autoplay=1"
+			"source": "https://www.dailymotion.com/embed/video/x7x4dgx?autoplay=1"
 		}
 	},
 ];

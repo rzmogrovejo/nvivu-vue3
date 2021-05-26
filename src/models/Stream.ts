4e3446d1362dea749625db8f2f2391617f212fdc
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-25 19:00:54 -0500

    refactor Stream with new models

--- src/models/Stream.ts
+++ src/models/Stream.ts
@@ -1,17 +1,15 @@
 import { nextTick } from '@vue/runtime-core';
 import axios from 'axios';
 import Hls from 'hls.js';
-import Channel from '../contracts/Channel';
-import snakeToCamel from '../utils/snakeToCamel';
-
-interface HtmlTags { 'iframe': string; 'video': string }
+import RawChannel from '../contracts/RawChannel';
+import Channel from './Channel';
 
 export default class Stream {
 	private html: string;
 	private toggle: boolean;
 
 	constructor() {
-		this.html = "";
+		this.html = '';
 		this.toggle = false;
 	}
 
@@ -28,32 +26,50 @@ export default class Stream {
 	}
 
 	private emptyHtml() {
-		this.html = "";
+		this.html = '';
 	}
 
-	private resolveSource(channel: Channel) {
+	public setHtmlContent(contentType: string, source?: string) {
+		this.html = (this.htmlContent(source) as any)[contentType].trim();
+	}
+
+	private resolveSource(channel: RawChannel) {
 		const condition = channel.stream.allow && 'url' in channel.stream;
 		return condition ? channel.stream.url! : channel.url;
 	}
 
-	private resolveHtml(channel: Channel, source: string) {
-		const condition = 'type' in channel.stream;
-		this.html = condition ? this.htmlTags(source)[channel.stream.type as keyof HtmlTags].trim() : '';
-	}
-
-	public resolveStream(channel: Channel) {
-
-		this.emptyHtml();
-
-		const slugToCamel = snakeToCamel(channel.slug);
-
-		if (typeof (this as any)[slugToCamel] === "function") {
-			(this as any)[slugToCamel](channel);
+	private resolveHtml(channel: Channel, source?: string) {		
+		if (!channel.content().allow()) {
+			this.emptyHtml();
 			return;
 		}
 
-		const source = this.resolveSource(channel);
-		this.resolveHtml(channel, source);
+		source = source || channel.content().source();
+
+		this.setHtmlContent(channel.content().type(), source);
+	}
+
+	public resolveStream(rawChannel: RawChannel) {
+		this.emptyHtml();
+		this.setHtmlContent('loading');
+
+		const channel = new Channel(rawChannel);
+
+		const slugInCamel = channel.slugInCamel();
+
+		if (typeof (this as any)[slugInCamel] === "function") {
+			(this as any)[slugInCamel](channel);
+			return;
+		}
+
+		if (!channel.content().allow()) {
+			this.setHtmlContent('blank', channel.source())
+			window.open(channel.source(), '_blank');
+			return;
+		}
+
+		this.resolveHtml(channel);
+		this.setToggle(channel.content().allow())
 	}
 
 	private async atv(channel: Channel) {
@@ -69,7 +85,7 @@ export default class Stream {
 	private async resolveAtv(channel: Channel, tokenEndpoint: string) {
 		// i'm going to fetch the script, run it and get the hash
 		const html = await axios
-			.get('https://url-scrapper-v1.herokuapp.com/?url=' + channel.url)
+			.get('https://url-scrapper-v1.herokuapp.com/?url=' + channel.source())
 			.then(response => response.data.html);
 		
 		const startHashScript = html.indexOf('_0x5d50') - 4; // 56852 - 4
@@ -88,7 +104,8 @@ export default class Stream {
 
 		// if the script returns undefined then a fallback should be applied
 		if (hashScriptValue == undefined) {
-			this.html = this.htmlTags(channel.url)['iframe'].trim();
+			this.setHtmlContent('blank', channel.source())
+			window.open(channel.source(), '_blank');
 			return;
 		}
 
@@ -97,7 +114,7 @@ export default class Stream {
 			.get(tokenEndpoint + hashScriptValue)
 			.then(response => response.data.token);
 
-		const source = this.resolveSource(channel) + token;
+		const source = channel.content().source() + token;
 		this.resolveHtml(channel, source);		
 
 		nextTick(() => {
@@ -109,10 +126,10 @@ export default class Stream {
 			} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 				video.src = source;
 			}
-		})
+		});
 	}
 
-	private htmlTags(url?: string): HtmlTags {
+	private htmlContent(source?: string) {
 		return {
 			'iframe': `
 				<iframe 
@@ -120,7 +137,7 @@ export default class Stream {
 					style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" 
 					frameborder="0" 
 					type="text/html" 
-					src="${ url }"
+					src="${ source }"
 					width="100%" 
 					height="100%"
 					allowfullscreen 
@@ -130,10 +147,34 @@ export default class Stream {
 			'video': `
 				<video 
 					id="video"
-					style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;" 
+					style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"
 					autoplay
 					controls>
 				</video>			
+			`,
+			'blank': `
+				<div
+					style="
+						color: white;
+    					margin: 0;
+    					position: absolute;
+    					top: 50%;
+    					left: 50%;
+    					transform: translate(-50%, -50%);">
+					Transmisión en un nueva ventana
+				</div>
+			`,
+			'loading': `
+				<div
+					style="
+						color: white;
+    					margin: 0;
+    					position: absolute;
+    					top: 50%;
+    					left: 50%;
+    					transform: translate(-50%, -50%);">
+					Cargando...
+				</div>
 			`
 		}
 	}

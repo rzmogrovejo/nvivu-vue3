89ebcb919a581c12e410c8a6f30016a68a29c970
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-26 22:31:29 -0500

    add util to manage urlParams

---
+++ src/utils/urlParam.ts
@@ -0,0 +1,13 @@
+export function insertUrlParam(key: any, value:any) {
+	const searchParams = new URLSearchParams(window.location.search);
+	searchParams.set(key, value);
+	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+	window.history.pushState({path: newurl}, '', newurl);
+}
+
+export function deleteUrlParam(key: any) {
+	const searchParams = new URLSearchParams(window.location.search);
+	searchParams.delete(key);
+	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+	window.history.pushState({ path: newurl }, '', newurl);
+}

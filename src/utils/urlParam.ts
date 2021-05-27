277776ce1451b8ad95fcf53d7f4a3cceb653a2f5
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-26 22:40:06 -0500

    change push to replace

--- src/utils/urlParam.ts
+++ src/utils/urlParam.ts
@@ -2,12 +2,12 @@ export function insertUrlParam(key: any, value:any) {
 	const searchParams = new URLSearchParams(window.location.search);
 	searchParams.set(key, value);
 	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
-	window.history.pushState({path: newurl}, '', newurl);
+	window.history.replaceState({ path: newurl }, '', newurl);
 }
 
 export function deleteUrlParam(key: any) {
 	const searchParams = new URLSearchParams(window.location.search);
 	searchParams.delete(key);
 	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
-	window.history.pushState({ path: newurl }, '', newurl);
+	window.history.replaceState({ path: newurl }, '', newurl);
 }

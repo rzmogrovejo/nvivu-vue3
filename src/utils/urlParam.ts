85bc7abad9dbf36d2910001cda869988ff687e7d
Renzo Mogrovejo <rzmogrovejo@Renzos-MacBook-Pro.local>
2021-05-26 23:34:14 -0500

    refactor functions

--- src/utils/urlParam.ts
+++ src/utils/urlParam.ts
@@ -1,13 +1,17 @@
-export function insertUrlParam(key: any, value:any) {
+export function insertUrlParam(key: string, value: string) {
 	const searchParams = new URLSearchParams(window.location.search);
 	searchParams.set(key, value);
-	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
-	window.history.replaceState({ path: newurl }, '', newurl);
+	replaceState(searchParams);
 }
 
-export function deleteUrlParam(key: any) {
+export function deleteUrlParam(key: string) {
 	const searchParams = new URLSearchParams(window.location.search);
 	searchParams.delete(key);
-	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + searchParams.toString();
+	replaceState(searchParams);
+}
+
+function replaceState(searchParams: URLSearchParams) {
+	const search = searchParams.toString() == '' ? '' : '?' + searchParams.toString();
+	const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + search;
 	window.history.replaceState({ path: newurl }, '', newurl);
 }

46a7cbe5c35f15c4179ca0bb97fd5e41f23f17f5
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2021-06-12 21:58:05 -0500

    add loadingType

---
+++ src/components/LoadingType.vue
@@ -0,0 +1,22 @@
+<template>
+	<div
+		style="
+			color: white;
+			margin: 0;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			text-align: center;" class="font-light">
+		Cargando...
+	</div>	
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: 'LoadingType',
+})
+</script>

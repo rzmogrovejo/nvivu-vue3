d5bbe7b89e348cabd9dba77a05e1b3336c241dc7
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2021-06-16 21:51:12 -0500

    refact repository

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,25 @@
 </template>
 
 <script lang="ts">
-import axios from "axios";
 import { defineComponent } from "vue";
+import Channels from '@/repo/Channels';
 import RawChannel from "./contracts/RawChannel";
 
 export default defineComponent({
 	name: 'App',
 	data() {
 		return {
-			rawChannels: []
+			rawChannels: Array()
 		}
 	},
 	async created() {
-		if (process.env.NODE_ENV === 'production') {
-			await this.resolveForProduction();
-		} else {
-			this.resolveForLocal();
-		}
+		this.rawChannels = await Channels.getAll((channels: RawChannel[]) => {
+			return channels.filter((channel: RawChannel) => channel.contentEnabled);
+		});
 	},
 	mounted() {
 		console.log('app');
 	},
-	methods: {
-		resolveForLocal() {
-			const rawChannels = require('@/data/raw-channelsv2.json');
-			this.rawChannels = this.filterChannels(rawChannels);
-		},
-		async resolveForProduction() {
-			let endpoint = "https://raw.githubusercontent.com/rzmogrovejo/nvivu/main/src/data/raw-channelsv2.json";
-
-			this.rawChannels = await axios(endpoint)
-				.then((response) => this.filterChannels(response.data));			
-		},
-		filterChannels(rawChannels: []) {
-			return rawChannels.filter((channel: RawChannel) => channel.contentEnabled);
-		}
-	}
 })
 </script>
 
---
+++ src/repo/Channels.ts
@@ -0,0 +1,52 @@
+import RawChannel from "@/contracts/RawChannel";
+import axios from "axios";
+import { cacheAdapterEnhancer } from "axios-extensions";
+
+export default class Channels {
+
+	static async getAll(): Promise<RawChannel[]>;
+
+	static async getAll(
+		callback: (channels: RawChannel[]) => RawChannel[]
+	): Promise<RawChannel[]>;
+
+	static async getAll(
+		callback?: (channels: RawChannel[]) => RawChannel[]
+	): Promise<RawChannel[]> {
+
+		if (!callback) {
+			callback = (channels: RawChannel[]) => {
+				return channels;
+			};
+		}
+
+		if (process.env.NODE_ENV !== 'production') {
+			return callback(await this.resolveForProduction());
+		} else {
+			return callback(this.resolveForLocal());
+		}
+	}	
+
+	static resolveForLocal(): RawChannel[] {
+		const rawChannels = require('@/data/raw-channelsv2');
+
+		if (!rawChannels) {
+			return [];
+		}
+
+		return rawChannels;
+	}
+
+	static async resolveForProduction(): Promise<RawChannel[]> {
+		const endpoint = "https://raw.githubusercontent.com/rzmogrovejo/nvivu/main/src/data/raw-channelsv2.json";
+
+		const http = axios.create({
+			baseURL: '/',
+			//headers: { 'Cache-Control': 'no-cache' },
+			// cache will be enabled by default
+			adapter: cacheAdapterEnhancer(axios.defaults.adapter!)
+		});
+
+		return await http(endpoint).then((response) => response.data);
+	}
+}

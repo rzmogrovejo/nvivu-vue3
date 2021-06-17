d1d7d8ceb91460ad57a12989aff5cd270215808b
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2021-06-17 01:44:28 -0500

    remove data fetching

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
-	<router-view :raw-channels="rawChannels"/>
+	<router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Channels from '@/repo/Channels';
-import RawChannel from "./contracts/RawChannel";
 
 export default defineComponent({
 	name: 'App',
-	data() {
-		return {
-			rawChannels: Array()
-		}
-	},
-	async created() {
-		this.rawChannels = await Channels.getAll((channels: RawChannel[]) => {
-			return channels.filter((channel: RawChannel) => channel.contentEnabled);
-		});
-	},
 	mounted() {
 		console.log('app');
 	},

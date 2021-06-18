ddeca87012e8c767614b9d223fe86e0a74aa9347
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2021-06-18 16:59:57 -0500

    add youtube component

---
+++ src/components/YoutubeType.vue
@@ -0,0 +1,30 @@
+<template>
+	<iframe-type :source="sourceWithParams"></iframe-type>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { defineComponent } from "vue";
+import IframeType from "@/components/IframeType.vue"
+
+export default defineComponent({
+	name: 'YoutubeType',
+	props: {
+		source: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			sourceWithParams: ''
+		}
+	},
+	created() {
+		this.sourceWithParams = this.source + "?autoplay=1&controls=1&modestbranding=1&rel=0";
+	},
+	components: {
+		IframeType
+	}
+})
+</script>

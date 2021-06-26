95c7ad1bd67864adc1de645523b3211853cf060f
Renzo Mogrovejo <rzmogrovejo@gmail.com>
2021-06-25 23:51:47 -0500

    update baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/nvivu/'
-    : '/'
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/nvivu-vue3/'
+		: '/'
 }
